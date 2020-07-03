7984832e9acb4282542b87e3b297ac030123fc4c
Nikita <nikitos_athletos@mail.ru>
2020-07-04 00:14:46 +0500

    added gmail page class

--- main.py
+++ main.py
@@ -29,27 +29,34 @@ class BasePage:
 		return self.driver.page_source
 
 
+class GMailPage(BasePage):
+
+	def login(self, mail, password):
+		self.click_on_element(By.XPATH, '//*[@id="openid-buttons"]/button[1]')
+		sleep(1)
+		self.input_data(By.NAME, 'identifier', mail)
+		self.click_on_element(By.ID, 'identifierNext')
+		sleep(1)
+		self.input_data(By.NAME, 'password', password)
+		self.click_on_element(By.ID, 'passwordNext')
+
+
 class GoogleTest(unittest.TestCase):
 	mail = settings.EMAIL_FOR_LOG_IN
 	password = settings.PASSWORD_FOR_LOG_IN
 
 	def setUp(self) -> None:
-		self.driver = BasePage(webdriver.Chrome)
+		self.site = GMailPage(webdriver.Chrome)
 
 	def test_login_google(self):
-		self.driver.go_to_site(
+		self.site.go_to_site(
 			'https://stackoverflow.com/users/signup?ssrc=head&returnurl=%2fusers%2fstory%2fcurrent%27')
+		sleep(1)
+		self.site.login(GoogleTest.mail, GoogleTest.password)
 		sleep(2)
-		self.driver.click_on_element(By.XPATH, '//*[@id="openid-buttons"]/button[1]')
-		self.driver.input_data(By.NAME, 'identifier', GoogleTest.mail)
-		self.driver.click_on_element(By.ID, 'identifierNext')
-		sleep(2)
-		self.driver.input_data(By.NAME, 'password', GoogleTest.password)
-		self.driver.click_on_element(By.ID, 'passwordNext')
-		sleep(2)
-		self.driver.go_to_site('https://www.google.com/gmail/')
+		self.site.go_to_site('https://www.google.com/gmail/')
 		sleep(4)
-		self.assertIn('Входящие', self.driver.get_page_source())
+		self.assertIn('Входящие', self.site.get_page_source())
 
 	def test_count_mail(self):
 		pass

b490b571d3506e452eb64999c65696107a64b0a9
Nikita <nikitos_athletos@mail.ru>
2020-07-04 00:02:27 +0500

    added login test

--- main.py
+++ main.py
@@ -1,27 +1,61 @@
 from time import sleep
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import settings
 
-class google_test(unittest.TestCase):
+
+class BasePage:
+
+	def __init__(self, driver):
+		self.driver = driver()
+
+	def go_to_site(self, url):
+		self.driver.get(url)
+
+	def click_on_element(self, by, element):
+		element_on_click = self.driver.find_element(by, element)
+		element_on_click.click()
+
+	def close_site(self):
+		self.driver.close()
+
+	def input_data(self, by, path, data):
+		input = self.driver.find_element(by, path)
+		input.send_keys(data)
+
+	def get_page_source(self):
+		return self.driver.page_source
+
+
+class GoogleTest(unittest.TestCase):
+	mail = settings.EMAIL_FOR_LOG_IN
+	password = settings.PASSWORD_FOR_LOG_IN
 
 	def setUp(self) -> None:
-		self.driver = webdriver.Chrome()
+		self.driver = BasePage(webdriver.Chrome)
 
 	def test_login_google(self):
-		self.driver.get('https://stackoverflow.com/users/signup?ssrc=head&returnurl=%2fusers%2fstory%2fcurrent%27')
+		self.driver.go_to_site(
+			'https://stackoverflow.com/users/signup?ssrc=head&returnurl=%2fusers%2fstory%2fcurrent%27')
 		sleep(2)
-		self.driver.find_element_by_xpath('//*[@id="openid-buttons"]/button[1]').click()
-		self.driver.find_element_by_xpath('//input[@type="email"]').send_keys(settings.EMAIL_FOR_LOG_IN)
-		self.driver.find_element_by_xpath('//*[@id="identifierNext"]').click()
+		self.driver.click_on_element(By.XPATH, '//*[@id="openid-buttons"]/button[1]')
+		self.driver.input_data(By.NAME, 'identifier', GoogleTest.mail)
+		self.driver.click_on_element(By.ID, 'identifierNext')
 		sleep(2)
-		self.driver.find_element_by_xpath('//input[@type="password"]').send_keys(settings.PASSWORD_FOR_LOG_IN)
-		self.driver.find_element_by_xpath('//*[@id="passwordNext"]').click()
+		self.driver.input_data(By.NAME, 'password', GoogleTest.password)
+		self.driver.click_on_element(By.ID, 'passwordNext')
 		sleep(2)
-		self.driver.get('https://www.google.com/gmail/')
+		self.driver.go_to_site('https://www.google.com/gmail/')
 		sleep(4)
-		self.driver.close()
+		self.assertIn('Входящие', self.driver.get_page_source())
+
+	def test_count_mail(self):
+		pass
+
+	def tearDown(self):
+		self.driver.close_site()
 
 
 if __name__ == "__main__":
